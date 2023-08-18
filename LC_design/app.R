0b5c85d52a946ccb16d567b1cdc4565a5a256fdb
Andrew Rate <andrew.rate@uwa.edu.au>
2023-08-18 09:24:58 +0800

    file now reads to sf object, removed data table

--- LC_design/app.R
+++ LC_design/app.R
@@ -10,7 +10,6 @@ ui <- fluidPage(
       # inputs
       fileInput("reserve", "Reserve boundary file", accept = ".csv"),
       numericInput("n", "Show this many rows of coordinates:", value = 5, min = 1, step = 1),
-      tableOutput("head")
     ),
     mainPanel(
       plotOutput("lcmap", width="70%", height = "600px")
@@ -21,17 +20,14 @@ ui <- fluidPage(
 server <- function(input, output, session) {
   data <- reactive({
     req(input$reserve)
-    vroom::vroom(input$reserve$datapath, delim = ",")
+    st_read(input$reserve$datapath) |>
+      st_as_sf(coords=c("E","N"), crs=st_crs(32750))
   })
   output$lcmap <- renderPlot({
-    par(mar=c(3,3,.5,.5), mgp=c(1.7,0.3,0),tcl=0.2)
-    plot(data(), asp=1, type="l", lwd=2, col="#ffa08080",
-         xlab = expression(bold("Easting, UTM Zone 50S")),
-         ylab = expression(bold("Northing, UTM Zone 50S")),
-         cex.lab=1.4)
-  })
-  output$head <- renderTable({
-    head(data(), input$n)
+    par(mar=c(3,3,.5,.5), mgp=c(1.7,0.3,0),tcl=0.2, font.lab=2)
+    plot(data(), type="l", lwd=2, col="#ffa08080",
+         xlab = "Easting, UTM Zone 50S", ylab = "Northing, UTM Zone 50S",
+         cex.lab=1.4, axes=T)
   })
 }
 
