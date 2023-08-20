2d324956df1c61eae161700b111d5c5304b807ee
Andrew Rate <andrew.rate@uwa.edu.au>
2023-08-20 18:10:26 +0800

    might be useful so pushed here

---
+++ R/testing code before including in shiny.R
@@ -0,0 +1,26 @@
+lcb <- read.csv("./arrange2/LC_SampledArea.csv", stringsAsFactors = TRUE)
+lcw <- read.csv("./arrange2/LC_WaterEdge.csv", stringsAsFactors = TRUE)
+lcpoly <- st_polygon(list(as.matrix(lcb),as.matrix(lcw))) |>
+  st_sfc(crs=st_crs(32750))
+
+gap <- 80
+off0 <- 10
+issq <- FALSE
+input <- data.frame(xsp=gap, ysp=gap)
+sampgrid <-st_make_grid(lcpoly,
+               cellsize=c(input$xsp,input$ysp),
+               offset = c(off0,off0),
+               what="centers", square=issq)
+
+ggplot(lcpoly) +
+    geom_sf(data=lcpoly, bg="#e0c08040", col="#806020") +
+    geom_sf(data=st_intersection(sampgrid, lcpoly),
+            shape=3, col="darkgoldenrod", size=3, stroke=2) +
+    xlab(label = "Longitude") +
+    ylab(label = "Latitude") +
+    geom_text(aes(x=384400, y=6461800, label="Lake\nClaremont",
+                  fontface = "italic"), col="royalblue", size=6) +
+    theme_bw() +
+    theme(axis.title = element_text(size=14, face = "bold"),
+          axis.text = element_text(size=12)) +
+    coord_sf(crs = st_crs(32750))
