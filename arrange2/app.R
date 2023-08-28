af2a76148785faac1e09ce5de8f367517a184b85
Andrew Rate <andrew.rate@uwa.edu.au>
2023-08-28 10:28:17 +0800

    now testing tabsetPanels

--- arrange2/app.R
+++ arrange2/app.R
@@ -5,61 +5,72 @@ library(sf)
 
 lcb <- read.csv("LC_SampledArea.csv", stringsAsFactors = TRUE)
 lcw <- read.csv("LC_WaterEdge.csv", stringsAsFactors = TRUE)
+lake <- st_polygon(list(as.matrix(lcw))) |> st_sfc(crs=st_crs(32750))
 lcpoly <- st_polygon(list(as.matrix(lcb),as.matrix(lcw))) |>
   st_sfc(crs=st_crs(32750))
 
 # ui.R ----
 ui <- fluidPage(
-  titlePanel("Sample planning map: Lake Claremont"),
+  titlePanel("Sampling grid illustration map"),
+  h3("Lake Claremont"),
   sidebarLayout(
     sidebarPanel(
       sliderInput(inputId = "xysp",
                   label = "Grid spacing (metres)",
                   min=30, max=200, value= 80),
-      sliderInput(inputId = "xyadj",
+      sliderInput(inputId = "off0",
                   label = "Offset from origin (metres)",
                   min=0, max=50, value= 0.001),
       selectInput(inputId="ifsq",
                   label="Do you want a rectangular grid?\n(triangular otherwise)",
                   choices=c("Yes"="TRUE", "No"="FALSE"), selected="TRUE"),
-      sliderInput(inputId = "xyjit",
-                  label = "Amount of randomness",
-                  min=0, max=0.5, value=0)
+      sliderInput(inputId = "rand0",
+                  label = "Amount of randomness (metres)",
+                  min=0, max=25, value= 0),
+      textOutput(outputId = "GridInfo")
     ),
     mainPanel(
-      plotOutput("lcmap", height = "640px")
+      tabsetPanel(
+       plotOutput("lcmap", height = "640px"),
+       dataTableOutput("samples")
+       )
+      )
     )
   )
-)
 
 
 # server.R ----
 server <- function(input, output) {
-  sg0 <- reactive({
-    st_coordinates(st_make_grid(lcpoly,
+  sampgrid <- reactive({
+    st_make_grid(lcpoly,
                  cellsize=c(input$xysp, input$xysp),
-                 offset = st_bbox(lcpoly)[c("xmin", "ymin")]+input$xyadj,
-                 what="centers", square=as.logical(input$ifsq)))
-    sg1 <- reactive({
-      data.frame(X=sg0[,1] + runif(nrow(sg0),0, xysp*xyjit),
-                 Y=sg0[,2] + runif(nrow(sg0),0, xysp*xyjit))
-      })
-    sampgrid <- st_as_sf(sg1, coords=c("X","Y"), crs=st_crs(32750))
+                 offset = st_bbox(lcpoly)[c("xmin", "ymin")]+input$off0,
+                 what="centers", square=as.logical(input$ifsq))
   })
   output$lcmap <- renderPlot(
     ggplot(lcpoly) +
+      geom_sf(data=lake, bg="#40c0ff40", col="#40c0ff40") +
       geom_sf(data=lcpoly, bg="#e0e08040", col="#808020") +
-      geom_sf(data=st_intersection(sampgrid(), lcpoly),
-              shape=10, col="#003087", size=3, stroke=1) +
-      xlab(label = "Longitude") +
-      ylab(label = "Latitude") +
+      geom_sf(data=st_as_sf(st_jitter(st_intersection(sampgrid(), lcpoly),
+                                      amount=input$rand0)),
+              shape=10, col="blue3", size=3, stroke=1) +
+      xlab(label = "Easting (m, UTM Zone 50S)") +
+      ylab(label = "Northing (m, UTM Zone 50S)") +
       geom_text(aes(x=384400, y=6461800, label="Lake\nClaremont",
                     fontface = "italic"), col="royalblue", size=6) +
       theme_bw() +
-      theme(axis.title = element_text(size=14, face = "bold"),
-            axis.text = element_text(size=12)) +
-      coord_sf(crs = st_crs(32750))
-    )
+      theme(axis.title = element_text(size=18, face = "bold"),
+            axis.text = element_text(size=14),
+            axis.ticks.length = unit(-0.2, "cm")) +
+      coord_sf(datum = st_crs(32750))
+  )
+  output$GridInfo <- renderText({
+    paste("Showing",NROW(st_intersection(sampgrid(), lcpoly)),"points")
+  })
+  output$samples <- renderDataTable(
+    st_coordinates(st_intersection(sampgrid(), lcpoly)),
+    options = list(pageLength = 10)
+  )
 }
 
 # Run the app ----
